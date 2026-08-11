72eb33fa7593e97bd951c54710eee9d8b08194c1
Vijay <vijayselvaraj27@gmail.com>
2026-08-11 11:34:33 +0530

    add product function

--- app.py
+++ app.py
@@ -2,5 +2,14 @@ def hello():
     return "Hello E-Commerce"
 
 
+def get_product():
+    return {
+        "id": 1,
+        "name": "Laptop",
+        "price": 50000
+    }
+
+
 if __name__ == "__main__":
     print(hello())
+    print(get_product())
